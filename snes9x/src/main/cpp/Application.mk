8f36f1ca6a2915c0fc8eddb74108d22420456141
Zash60 <220589629+Zash60@users.noreply.github.com>
2025-12-08 22:56:11 +0000

    Auto-fix: Disable Fortify Source, Gradle 8 & C++ Patches

--- snes9x/src/main/cpp/Application.mk
+++ snes9x/src/main/cpp/Application.mk
@@ -7,3 +7,4 @@ compile-s-source = $(eval $(call ev-compile-c-source,$1,$(1:%.s=%.o)))
 JNI_H_INCLUDE = $(APP_PROJECT_PATH)/common/libnativehelper/include/APP_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0
 APP_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0
 APP_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0
+APP_CFLAGS += -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0
--- snes9x/src/main/cpp/common/emulator.cpp
+++ snes9x/src/main/cpp/common/emulator.cpp
@@ -19,6 +19,9 @@
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
 #include <stdlib.h>
 #include <unistd.h>
 #include <dlfcn.h>
