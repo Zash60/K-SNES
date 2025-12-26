557d0731d13b4bb974ba6a95d18d9b8451bf03b2
Zash60 <xath06@gmail.com>
2025-12-26 20:13:37 +0000

    aafffii

--- app/src/main/java/com/kansus/ksnes/activity/EmulatorActivity.java
+++ app/src/main/java/com/kansus/ksnes/activity/EmulatorActivity.java
@@ -20,7 +20,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
